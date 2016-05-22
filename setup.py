bb4bb8ac6b6d4239720c4d8f4d93616b55008790
Hannah Ward <Hannah.ward9001@gmail.com>
2016-05-22 17:39:07 +0100

    memes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	name = "Unique-Image-Sort",
 	version = "1.0",
 	description = "Sort through directories and find unique images",
-	requires = ['pil-or-pillow'],
+	requires = ['pillow'],
 	author = "Hannah Ward",
 	author_email = "hannah.ward9001@gmail.com",
 	scripts = ["unique-image-sort.py"]
